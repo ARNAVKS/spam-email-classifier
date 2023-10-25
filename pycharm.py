f8a238d9b1b66affacabda2d7127dab5871a741c
Arnav Kumar Shukla <134054312+ARNAVKS@users.noreply.github.com>
2023-10-25 15:23:55 +0530

    Arnav updated pycharm.py

--- pycharm.py
+++ pycharm.py
@@ -5,6 +5,11 @@ import streamlit as st
 import pickle
 import string
 import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+
+ps = PorterStemmer()
 
 st.title('Spam Email Classifier')
 
@@ -22,6 +27,20 @@ def punc(text):
         text = text.replace(i,'')
     return text
 
+def stop_remover(text):
+  new_text=[]
+  for i in text.split():
+    if i not in stopwords.words('english'):
+      new_text.append(i)
+  return " ".join(new_text)
+
+def stemmer(text):
+  new_text=[]
+  for i in text.split():
+    i = ps.stem(i)
+    new_text.append(i)
+  return " ".join(new_text)
+
 def transform_text(text):
   text = text.lower()
   text = newline_rem.sub('',text)
@@ -29,6 +48,8 @@ def transform_text(text):
   text = mail_rem.sub('',text)
   text = punc(text)
   text = num_remover(text)
+  text = stop_remover(text)
+  text = stemmer(text)  
   return text
 
 cv = pickle.load(open('vectorizer.pkl','rb'))
